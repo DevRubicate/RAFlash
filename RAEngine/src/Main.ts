5b3ad7c4c5ab971fa511a29c50c306c8815ffb70
DevRubicate <kimrobinlindblom@gmail.com>
2026-04-12 14:38:31 +0200

    Bump version to 0.0.12

--- RAEngine/src/Main.ts
+++ RAEngine/src/Main.ts
@@ -31,7 +31,7 @@ import * as pako from "npm:pako";
 import { unzipSync } from "npm:fflate";
 import { startSitelockProxy, stopSitelockProxy } from "./SitelockProxy.ts";
 
-const VERSION = "0.0.11";
+const VERSION = "0.0.12";
 
 // Compile a formula and emit an error log if compilation fails
 function compileFormula(input: string): unknown[] {
